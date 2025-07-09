28dd387bb3c18c47c39286f03445cd6722a1919c
F15 <duongquocthai368@gmail.com>
2025-07-09 22:14:37 +0700

    add first

---
+++ Thai.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Guessing Game - 48d75f4d</title>
+</head>
+<body>
+    <h1>Guessing Game</h1>
+
+    <?php
+    if (!isset($_GET['guess'])) {
+        echo "<p>Please enter a guess using the URL, like ?guess=42</p>";
+    } else {
+        $guess = $_GET['guess'];
+
+        if (strlen($guess) < 1) {
+            echo "<p>Your guess is too short.</p>";
+        } elseif (!is_numeric($guess)) {
+            echo "<p>Your guess is not a number.</p>";
+        } else {
+            $guess = (int)$guess;
+            $correct = 32;
+
+            if ($guess < $correct) {
+                echo "<p>Your guess is too low.</p>";
+            } elseif ($guess > $correct) {
+                echo "<p>Your guess is too high.</p>";
+            } else {
+                echo "<p>Congratulations - You are right!</p>";
+            }
+        }
+    }
+    ?>
+</body>
+</html>
